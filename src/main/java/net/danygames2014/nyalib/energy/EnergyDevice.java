252fad547c0fbddef1024d0d5080887e4c01c388
Daniel Fildán <fildand@centrum.cz>
2024-06-26 13:25:33 +0200

    Combined Provider & Receiver into EnergyDevice

---
+++ src/main/java/net/danygames2014/nyalib/energy/EnergyDevice.java
@@ -0,0 +1,39 @@
+package net.danygames2014.nyalib.energy;
+
+import net.modificationstation.stationapi.api.util.math.Direction;
+
+public interface EnergyDevice extends EnergyCapable{
+
+    /**
+     * @param direction Direction to check
+     * @return <code>true</code> if the device supports energy extraction from the given direction
+     */
+    boolean canExtractEnergy(Direction direction);
+
+    /**
+     * Extract energy from the device
+     *
+     * @param amount    Amount of energy to be extracted
+     * @param simulate  If <code>true</code> the action will only be simulated
+     * @param direction {@link Direction} from which the energy is extracted from
+     * @return Amount of energy that was (or would have been, if simulated) extracted
+     */
+    int extractEnergy(int amount, boolean simulate, Direction direction);
+
+
+    /**
+     * @param direction Direction to check
+     * @return <code>true</code> if the device supports energy insertion from the given direction
+     */
+    boolean canInsertEnergy(Direction direction);
+
+    /**
+     * Provide energy to the device
+     *
+     * @param amount    Amount of energy provided to the device
+     * @param simulate  If <code>true</code> the action will only be simulated
+     * @param direction {@link Direction} from which the energy is received
+     * @return Amount of energy that was (or would have been, if simulated) received
+     */
+    int insertEnergy(int amount, boolean simulate, Direction direction);
+}
