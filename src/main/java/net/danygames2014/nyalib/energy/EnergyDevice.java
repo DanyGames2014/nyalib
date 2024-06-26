c4d1b49ec862b0931c6a9bbf245566dffb1051ce
Daniel Fildán <fildand@centrum.cz>
2024-06-26 14:31:57 +0200

    Make Direction in energy methods nullable

--- src/main/java/net/danygames2014/nyalib/energy/EnergyDevice.java
+++ src/main/java/net/danygames2014/nyalib/energy/EnergyDevice.java
@@ -1,14 +1,16 @@
 package net.danygames2014.nyalib.energy;
 
 import net.modificationstation.stationapi.api.util.math.Direction;
+import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings({"UnusedReturnValue", "unused"})
 public interface EnergyDevice extends EnergyCapable{
 
     /**
      * @param direction Direction to check
      * @return <code>true</code> if the device supports energy extraction from the given direction
      */
-    boolean canExtractEnergy(Direction direction);
+    boolean canExtractEnergy(@Nullable Direction direction);
 
     /**
      * Extract energy from the device
@@ -18,14 +20,14 @@ public interface EnergyDevice extends EnergyCapable{
      * @param direction {@link Direction} from which the energy is extracted from
      * @return Amount of energy that was (or would have been, if simulated) extracted
      */
-    int extractEnergy(int amount, boolean simulate, Direction direction);
+    int extractEnergy(int amount, boolean simulate, @Nullable Direction direction);
 
 
     /**
      * @param direction Direction to check
      * @return <code>true</code> if the device supports energy insertion from the given direction
      */
-    boolean canInsertEnergy(Direction direction);
+    boolean canInsertEnergy(@Nullable Direction direction);
 
     /**
      * Provide energy to the device
@@ -35,5 +37,5 @@ public interface EnergyDevice extends EnergyCapable{
      * @param direction {@link Direction} from which the energy is received
      * @return Amount of energy that was (or would have been, if simulated) received
      */
-    int insertEnergy(int amount, boolean simulate, Direction direction);
+    int insertEnergy(int amount, boolean simulate, @Nullable Direction direction);
 }
