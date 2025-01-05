aa8fe834bd5c7a611d469792488f187448dd3e08
Daniel Fildán <fildand@centrum.cz>
2025-01-05 19:46:07 +0100

    Prepare for StationAPI Alpha 4

--- src/main/java/net/danygames2014/nyalib/NyaLib.java
+++ src/main/java/net/danygames2014/nyalib/NyaLib.java
@@ -4,15 +4,14 @@ import net.danygames2014.nyalib.config.Config;
 import net.glasslauncher.mods.gcapi3.api.ConfigRoot;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.util.Namespace;
-import net.modificationstation.stationapi.api.util.Null;
 import org.apache.logging.log4j.Logger;
 
 public class NyaLib {
     @Entrypoint.Logger
-    public static final Logger LOGGER = Null.get();
+    public static Logger LOGGER;
 
     @Entrypoint.Namespace
-    public static final Namespace NAMESPACE = Null.get();
+    public static Namespace NAMESPACE;
     
     @ConfigRoot(value = "item", visibleName = "Item API")
     public static final Config.ItemConfig ITEM_CONFIG = new Config.ItemConfig();
