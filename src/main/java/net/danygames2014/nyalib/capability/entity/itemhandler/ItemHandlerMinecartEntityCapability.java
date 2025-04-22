063c131689beeb454c25042b86121f492c9a410d
Daniel Fildán <fildand@centrum.cz>
2025-04-22 08:18:10 +0200

    Finish built in item handler entity capabilities

---
+++ src/main/java/net/danygames2014/nyalib/capability/entity/itemhandler/ItemHandlerMinecartEntityCapability.java
@@ -0,0 +1,101 @@
+package net.danygames2014.nyalib.capability.entity.itemhandler;
+
+import net.minecraft.entity.vehicle.MinecartEntity;
+import net.minecraft.item.ItemStack;
+
+public class ItemHandlerMinecartEntityCapability extends ItemHandlerEntityCapability {
+    MinecartEntity minecart;
+
+    public ItemHandlerMinecartEntityCapability(MinecartEntity minecart) {
+        this.minecart = minecart;
+    }
+
+    @Override
+    public boolean canExtractItem() {
+        return true;
+    }
+
+    @Override
+    public ItemStack extractItem(int slot, int amount) {
+        return minecart.removeStack(slot, amount);
+    }
+
+    @Override
+    public boolean canInsertItem() {
+        return true;
+    }
+
+    @Override
+    public ItemStack insertItem(ItemStack stack, int slot) {
+        // Check if the current stack is null
+        if (minecart.getStack(slot) == null) {
+            if (stack.count <= minecart.getMaxCountPerStack()) {
+                // If the stack fits into the slot, just set it and return null meaning the stack was inserted entirely
+                minecart.setStack(slot, stack);
+                return null;
+
+            } else if (stack.count > minecart.getMaxCountPerStack()) {
+                // If the stack is too large for the slot, insert a maximum size stack and then return a remainder stack
+                minecart.setStack(slot, stack);
+
+                ItemStack remainderStack = stack.copy();
+                remainderStack.count = stack.count - minecart.getMaxCountPerStack();
+                return remainderStack;
+            }
+        } else {
+            // Check if the existing stack is equal to the one we are trying to insert
+            ItemStack existingStack = minecart.getStack(slot);
+            if (existingStack.isItemEqual(stack)) {
+                int freeSpace = minecart.getMaxCountPerStack() - existingStack.count;
+
+                // Add to the existing stack, the underlying setStack method will make sure to limit the final amount
+                existingStack.count += stack.count;
+                minecart.setStack(slot, existingStack);
+
+                if (freeSpace >= stack.count) {
+                    // If there is enough space to fit the inserted stack, just add to the stack and return null
+                    return null;
+
+                } else {
+                    // If there isnt enough space, "fill" the stack up to maximum and return remainder
+                    ItemStack remainderStack = stack.copy();
+                    remainderStack.count = stack.count - freeSpace;
+                    return remainderStack;
+                }
+            }
+        }
+
+        return stack;
+    }
+
+    @Override
+    public ItemStack insertItem(ItemStack stack) {
+        ItemStack insertedStack = stack.copy();
+
+        for (int i = 0; i < minecart.size(); i++) {
+            insertedStack = insertItem(insertedStack, i);
+            
+            // If its null, it was inserted entirely, no need to loop furthers
+            if (insertedStack == null) {
+                return null;
+            }
+        }
+
+        return insertedStack;
+    }
+
+    @Override
+    public ItemStack getItemInSlot(int slot) {
+        return minecart.getStack(slot);
+    }
+
+    @Override
+    public int getItemSlots() {
+        return minecart.size();
+    }
+
+    @Override
+    public ItemStack[] getInventory() {
+        return minecart.inventory;
+    }
+}
