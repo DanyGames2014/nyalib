40ccf30d36edcfa3ff4170cbd79b0552add214b3
Daniel Fildán <fildand@centrum.cz>
2025-04-25 17:19:09 +0200

    Add 2 missing ItemHandler methods to ItemHandlerBlockCapability

--- src/main/java/net/danygames2014/nyalib/capability/block/itemhandler/ItemHandlerBlockCapability.java
+++ src/main/java/net/danygames2014/nyalib/capability/block/itemhandler/ItemHandlerBlockCapability.java
@@ -2,6 +2,7 @@ package net.danygames2014.nyalib.capability.block.itemhandler;
 
 import net.danygames2014.nyalib.capability.block.BlockCapability;
 import net.danygames2014.nyalib.item.ItemHandler;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.modificationstation.stationapi.api.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
@@ -15,6 +16,16 @@ public abstract class ItemHandlerBlockCapability extends BlockCapability impleme
 
     @Override
     public abstract ItemStack extractItem(int slot, int amount, @Nullable Direction direction);
+
+    @Override
+    public ItemStack extractItem(@Nullable Direction direction) {
+        return ItemHandler.super.extractItem(direction);
+    }
+
+    @Override
+    public ItemStack extractItem(Item item, int amount, @Nullable Direction direction) {
+        return ItemHandler.super.extractItem(item, amount, direction);
+    }
 
     @Override
     public abstract boolean canInsertItem(@Nullable Direction direction);
