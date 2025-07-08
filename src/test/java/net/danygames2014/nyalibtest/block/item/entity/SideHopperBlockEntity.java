0779f09dffc8843330eeb3d86eaccc639ee84e08
Daniel Fildán <fildand@centrum.cz>
2025-07-08 15:01:46 +0200

    [WIP] Port Side Hopper example to capabilities

--- src/test/java/net/danygames2014/nyalibtest/block/item/entity/SideHopperBlockEntity.java
+++ src/test/java/net/danygames2014/nyalibtest/block/item/entity/SideHopperBlockEntity.java
@@ -1,5 +1,8 @@
 package net.danygames2014.nyalibtest.block.item.entity;
 
+import net.danygames2014.nyalib.capability.Capability;
+import net.danygames2014.nyalib.capability.CapabilityHelper;
+import net.danygames2014.nyalib.capability.block.itemhandler.ItemHandlerBlockCapability;
 import net.danygames2014.nyalib.item.ItemHandler;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
@@ -20,17 +23,28 @@ public class SideHopperBlockEntity extends BlockEntity {
     @Override
     public void tick() {
         if (tickCounter <= 0) {
-            if(facing == null){
-                facing = world.getBlockState(x,y,z).get(Properties.HORIZONTAL_FACING);
+            if (facing == null) {
+                facing = world.getBlockState(x, y, z).get(Properties.HORIZONTAL_FACING);
             }
 
-            if(world.getBlockEntity(x + facing.getOffsetX(), y, z + facing.getOffsetZ()) instanceof ItemHandler south && world.getBlockEntity(x + facing.getOpposite().getOffsetX(), y, z + facing.getOpposite().getOffsetZ()) instanceof ItemHandler north){
-                // If there is nothing in internal buffer, try to retrive item
-                if (internalBuffer == null){
-                    if(south.canExtractItem(facing.getOpposite())){
-                        for (int i = 0; i < south.getItemSlots(facing.getOpposite()); i++) {
-                            internalBuffer = south.extractItem(i, 999, facing.getOpposite());
-                            if(internalBuffer != null){
+            ItemHandlerBlockCapability source = CapabilityHelper.getCapability(world, x + facing.getOffsetX(), y, z + facing.getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
+            ItemHandlerBlockCapability destination = CapabilityHelper.getCapability(world, x + facing.getOpposite().getOffsetX(), y , facing.getOpposite().getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
+            
+            if (source != null) {
+                if (internalBuffer == null && source.canExtractItem(facing.getOpposite())) {
+                    internalBuffer = source.extractItem(facing.getOpposite());
+                }
+            }
+            
+            
+
+            if (world.getBlockEntity(x + facing.getOffsetX(), y, z + facing.getOffsetZ()) instanceof ItemHandler sourcex && world.getBlockEntity(x + facing.getOpposite().getOffsetX(), y, z + facing.getOpposite().getOffsetZ()) instanceof ItemHandler destinationx) {
+                // If there is nothing in internal buffer, try to retrieve item
+                if (internalBuffer == null) {
+                    if (sourcex.canExtractItem(facing.getOpposite())) {
+                        for (int i = 0; i < sourcex.getItemSlots(facing.getOpposite()); i++) {
+                            internalBuffer = sourcex.extractItem(i, 999, facing.getOpposite());
+                            if (internalBuffer != null) {
                                 break;
                             }
                         }
@@ -38,9 +52,9 @@ public class SideHopperBlockEntity extends BlockEntity {
                 }
 
                 // If there is something in internal buffer, try to get rid of it
-                if(internalBuffer != null){
-                    if(north.canInsertItem(facing)){
-                        internalBuffer = north.insertItem(internalBuffer, facing);
+                if (internalBuffer != null) {
+                    if (destinationx.canInsertItem(facing)) {
+                        internalBuffer = destinationx.insertItem(internalBuffer, facing);
                     }
                 }
             }
