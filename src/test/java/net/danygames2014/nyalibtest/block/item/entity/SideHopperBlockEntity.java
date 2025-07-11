6bd70ec10362c40ca77045cc5052ae1f47a0c735
Daniel Fildán <fildand@centrum.cz>
2025-07-11 09:03:13 +0200

    Fix side hopper and make it use capabilities

--- src/test/java/net/danygames2014/nyalibtest/block/item/entity/SideHopperBlockEntity.java
+++ src/test/java/net/danygames2014/nyalibtest/block/item/entity/SideHopperBlockEntity.java
@@ -27,35 +27,18 @@ public class SideHopperBlockEntity extends BlockEntity {
                 facing = world.getBlockState(x, y, z).get(Properties.HORIZONTAL_FACING);
             }
 
-            ItemHandlerBlockCapability source = CapabilityHelper.getCapability(world, x + facing.getOffsetX(), y, z + facing.getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
-            ItemHandlerBlockCapability destination = CapabilityHelper.getCapability(world, x + facing.getOpposite().getOffsetX(), y , facing.getOpposite().getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
+            ItemHandlerBlockCapability source = CapabilityHelper.getCapability(world, x + facing.getOpposite().getOffsetX(), y, z + facing.getOpposite().getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
+            ItemHandlerBlockCapability destination = CapabilityHelper.getCapability(world, x + facing.getOffsetX(), y , z + facing.getOffsetZ(), Capability.ITEM_HANDLER_CAPABILITY);
             
             if (source != null) {
-                if (internalBuffer == null && source.canExtractItem(facing.getOpposite())) {
-                    internalBuffer = source.extractItem(facing.getOpposite());
+                if (internalBuffer == null && source.canExtractItem(facing)) {
+                    internalBuffer = source.extractItem(facing);
                 }
             }
             
-            
-
-            if (world.getBlockEntity(x + facing.getOffsetX(), y, z + facing.getOffsetZ()) instanceof ItemHandler sourcex && world.getBlockEntity(x + facing.getOpposite().getOffsetX(), y, z + facing.getOpposite().getOffsetZ()) instanceof ItemHandler destinationx) {
-                // If there is nothing in internal buffer, try to retrieve item
-                if (internalBuffer == null) {
-                    if (sourcex.canExtractItem(facing.getOpposite())) {
-                        for (int i = 0; i < sourcex.getItemSlots(facing.getOpposite()); i++) {
-                            internalBuffer = sourcex.extractItem(i, 999, facing.getOpposite());
-                            if (internalBuffer != null) {
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                // If there is something in internal buffer, try to get rid of it
-                if (internalBuffer != null) {
-                    if (destinationx.canInsertItem(facing)) {
-                        internalBuffer = destinationx.insertItem(internalBuffer, facing);
-                    }
+            if (destination != null) {
+                if (internalBuffer != null && destination.canInsertItem(facing.getOpposite())) {
+                    internalBuffer = destination.insertItem(internalBuffer, facing.getOpposite());
                 }
             }
 
