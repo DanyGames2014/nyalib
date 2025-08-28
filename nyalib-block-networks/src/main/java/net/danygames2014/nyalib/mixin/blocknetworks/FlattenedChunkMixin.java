406935769de57b05777a04407d804f3b1a343077
alpha <alphamodeyt@gmail.com>
2025-08-28 13:48:41 -0500

    Fix cursed FlattenedChunkMixin I've spent 4 hours debugging loom for this (I give up)

--- nyalib-block-networks/src/main/java/net/danygames2014/nyalib/mixin/blocknetworks/FlattenedChunkMixin.java
+++ nyalib-block-networks/src/main/java/net/danygames2014/nyalib/mixin/blocknetworks/FlattenedChunkMixin.java
@@ -1,26 +1,27 @@
 package net.danygames2014.nyalib.mixin.blocknetworks;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.danygames2014.nyalib.network.NetworkComponent;
 import net.minecraft.block.Block;
-import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.impl.world.chunk.FlattenedChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = FlattenedChunk.class)
+@Mixin(FlattenedChunk.class)
 public class FlattenedChunkMixin {
     // This exists because StationAPI still calls the onBlockPlaced method even when setBlockState is used to update the current state's properties
     // That call results in onPlaced getting called on the Network Component, invoking the creation of a new network
-    @WrapWithCondition(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onBlockPlaced(Lnet/minecraft/world/World;IIILnet/modificationstation/stationapi/api/block/BlockState;)V"), require = 0)
-    public boolean aaa(Block block, World world, int x, int y, int z, BlockState oldState) {
+    @ModifyExpressionValue(method = "setBlockState", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;isRemote:Z"))
+    public boolean aaa(boolean original, @Local(argsOnly = true) BlockState state, @Local(ordinal = 1) BlockState oldState) {
         // If the block is a network component AND the old state is the same as the block
         //noinspection RedundantIfStatement
-        if (block instanceof NetworkComponent && oldState.isOf(block)) {
+        Block block = state.getBlock();
+        if (!(block instanceof NetworkComponent && oldState.isOf(block))) {
             // Supress the onBlockPlaced method call
-            return false;
+            return true;
         }
-        return true;
+        return original;
     }
 }
