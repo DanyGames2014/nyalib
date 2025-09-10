0a45fbbdad3a325f52f6cfc0d191f4e77f015b90
Daniel Fildán <fildand@centrum.cz>
2025-09-11 00:33:09 +0200

    [WIP] Fluid Overlay

--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/HeldItemRendererMixin.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/HeldItemRendererMixin.java
@@ -4,11 +4,16 @@ import net.danygames2014.nyalib.block.FlowingFluidBlock;
 import net.danygames2014.nyalib.block.StillFluidBlock;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.util.math.MathHelper;
+import net.modificationstation.stationapi.api.client.StationRenderAPI;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,9 +22,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class HeldItemRendererMixin {
     @Shadow
     private Minecraft minecraft;
-
-    @Shadow
-    protected abstract void renderUnderwaterOverlay(float delta);
 
     @Inject(method = "renderScreenOverlays", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glEnable(I)V", ordinal = 0, shift = At.Shift.BEFORE))
     public void renderFluidOverlay(float tickDelta, CallbackInfo ci) {
@@ -45,22 +47,36 @@ public abstract class HeldItemRendererMixin {
             return;
         }
 
-        int textureId = stillBlock.textureHolder.getStillTextureId();
+        int stillTextureId = stillBlock.textureHolder.getStillTextureId();
         //textureId = Atlases.getTerrain().getTexture(Identifier.of("nyalibtest:block/fuel_still")).index;
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
-        this.renderUnderwaterOverlay(tickDelta);
+        renderOverlay(tickDelta, stillTextureId);
+    }
 
-
-//        int atlasTextureId = this.minecraft.textureManager.getTextureId("/terrain.png");
-//        GL11.glBindTexture(3553, atlasTextureId);
-
-
-        //int waterTextureId = this.minecraft.textureManager.getTextureId("/misc/water.png");
-        //GL11.glBindTexture(3553, waterTextureId);
-//        this.renderUnderwaterOverlay(tickDelta);
-
-//        if (this.minecraft.player.isInFluid(Material.WATER)) {
-//
-//        }
+    @Unique
+    int atlasTextureId = -1;
+    
+    @Unique
+    private void renderOverlay(float tickDelta, int textureId) {
+        Atlas.Sprite tex = Atlases.getTerrain().getTexture(textureId);
+        
+        Tessellator tessellator = Tessellator.INSTANCE;
+        float brightness = this.minecraft.player.getBrightnessAtEyes(tickDelta);
+        if (atlasTextureId == - 1) {
+            atlasTextureId = StationRenderAPI.getBakedModelManager().getAtlas(Atlases.GAME_ATLAS_TEXTURE).getGlId();    
+        }
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, atlasTextureId);
+        GL11.glColor4f(brightness, brightness, brightness, 0.5F);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glPushMatrix();
+        tessellator.startQuads();
+        tessellator.vertex(-1.0f, -1.0f, -0.5f, tex.getEndU(), tex.getEndV());
+        tessellator.vertex(1.0f, -1.0f, -0.5f, tex.getStartU(), tex.getEndV());
+        tessellator.vertex(1.0f, 1.0f, -0.5f, tex.getStartU(), tex.getStartV());
+        tessellator.vertex(-1.0f, 1.0f, -0.5f, tex.getEndU(), tex.getStartV());
+        tessellator.draw();
+        GL11.glPopMatrix();
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GL11.glDisable(GL11.GL_BLEND);
     }
 }
