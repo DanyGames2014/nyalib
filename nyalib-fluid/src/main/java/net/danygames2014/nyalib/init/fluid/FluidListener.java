ebf07623cd1b614c6776243c06271e0029dfdc92
Daniel Fildán <fildand@centrum.cz>
2025-10-13 09:26:33 +0200

    Add default bucket items to Water and Lava

--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/init/fluid/FluidListener.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/init/fluid/FluidListener.java
@@ -5,17 +5,18 @@ import net.danygames2014.nyalib.fluid.Fluids;
 import net.danygames2014.nyalib.fluid.Fluid;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.modificationstation.stationapi.api.util.Namespace;
 
 public class FluidListener {
     @EventListener
     public void registerVanillaFluids(FluidRegistryEvent event) {
         event.register(
-                Fluids.WATER = new Fluid(Namespace.MINECRAFT.id("water"), Block.WATER, Block.FLOWING_WATER).setColor(0xFF3460DA).disableAutomaticBucketRegistration()
+                Fluids.WATER = new Fluid(Namespace.MINECRAFT.id("water"), Block.WATER, Block.FLOWING_WATER).setColor(0xFF3460DA).disableAutomaticBucketRegistration().setBucketItem(Item.WATER_BUCKET)
         );
         
         event.register(
-                Fluids.LAVA = new Fluid(Namespace.MINECRAFT.id("lava"), Block.LAVA, Block.FLOWING_LAVA).setColor(0xFFE6913C).disableAutomaticBucketRegistration()
+                Fluids.LAVA = new Fluid(Namespace.MINECRAFT.id("lava"), Block.LAVA, Block.FLOWING_LAVA).setColor(0xFFE6913C).disableAutomaticBucketRegistration().setBucketItem(Item.LAVA_BUCKET)
         );
     }
 }
