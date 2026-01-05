9953025967b2e8e6c47831cfdb37581c7512c041
Daniel Fildán <fildand@centrum.cz>
2026-01-05 15:06:48 +0100

    Rewrote screen handler fluid slot insertion/extraction logic

--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/ScreenHandlerMixin.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/ScreenHandlerMixin.java
@@ -1,15 +1,16 @@
 package net.danygames2014.nyalib.mixin.fluid;
 
+import net.danygames2014.nyalib.NyaLib;
 import net.danygames2014.nyalib.capability.CapabilityHelper;
 import net.danygames2014.nyalib.capability.item.fluidhandler.FluidHandlerItemCapability;
-import net.danygames2014.nyalib.fluid.FluidHandler;
-import net.danygames2014.nyalib.fluid.FluidSlot;
-import net.danygames2014.nyalib.fluid.FluidStack;
+import net.danygames2014.nyalib.fluid.*;
 import net.danygames2014.nyalib.screen.FluidScreenHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerListener;
@@ -92,64 +93,205 @@ public abstract class ScreenHandlerMixin implements FluidScreenHandler {
         if (index == -999) {
             return null;
         }
-        
+
         FluidSlot slot = fluidSlots.get(index);
 
         if (slot != null) {
-            FluidHandler handler = slot.getHandler();
+            FluidHandler inv = slot.getHandler();
 
             if (cursorStack == null) {
                 return slot.getStack();
             }
-            
-            FluidHandlerItemCapability cap = CapabilityHelper.getCapability(cursorStack, FluidHandlerItemCapability.class);
-            
-            if (cap != null) {
-                FluidStack fluid = cap.getFluid(0);
-                FluidStack invFluid = handler.getFluid(index, null);
-                if (fluid != null) {
-                    if (cap.canExtractFluid() && handler.canInsertFluid(null)) {
-                        if (invFluid == null) {
-                            FluidStack fluidStack = cap.extractFluid(handler.getRemainingFluidCapacity(index, null));
-                            FluidStack remainder = handler.insertFluid(fluidStack, index, null);
-                            if (remainder != null && remainder.amount > 0) {
-                                cap.insertFluid(remainder);
+
+            // Item Capability
+            FluidHandlerItemCapability item = CapabilityHelper.getCapability(cursorStack, FluidHandlerItemCapability.class);
+
+            if (item != null) {
+                FluidStack itemFluidStack = item.getFluid(0);
+                FluidStack invFluidStack = inv.getFluid(index, null);
+
+                if (itemFluidStack == null && invFluidStack == null) {
+                    // Both are empty. Do Nothing
+
+                } else if (itemFluidStack == null) {
+                    // The item is empty. Fill It
+                    FluidStack extractedStack = inv.extractFluid(index, item.getRemainingFluidCapacity(0), null);
+                    FluidStack remainderStack = item.insertFluid(extractedStack);
+                    if (remainderStack != null && remainderStack.amount > 0) {
+                        inv.insertFluid(remainderStack, index, null);
+                    }
+
+                } else { // if (invFluidStack == null) {
+                    // The inventory slot is empty. Fill It
+                    FluidStack extractedStack = item.extractFluid(0, inv.getRemainingFluidCapacity(index, null));
+                    FluidStack remainderStack = extractedStack;
+
+                    if (slot.canInsert(remainderStack)) {
+                        remainderStack = inv.insertFluid(extractedStack, index, null);
+                    }
+
+                    if (remainderStack != null && remainderStack.amount > 0) {
+                        item.insertFluid(remainderStack, 0);
+                    }
+
+                }
+            }
+
+            // Bucket
+            if (cursorStack.getItem() instanceof FluidBucket bucket) {
+                Fluid bucketFluid = bucket.getFluid();
+                FluidStack invFluidStack = inv.getFluid(index, null);
+
+                if (bucketFluid == null && invFluidStack == null) {
+                    // Both are empty. Do Nothing
+
+                } else if (bucketFluid == null) {
+                    // The bucket is empty, try to fill it
+
+                    // If the fluid has a bucket item, try to form it
+                    if (bucket.getFullBucketItem(invFluidStack.fluid) != null) {
+                        int iterationLimit = 127;
+                        while (iterationLimit-- > 0) {
+                            if (inv.getFluid(index, null) == null) {
+                                break;
                             }
-                        } else if (invFluid.amount <= handler.getFluidCapacity(0, null)) {
-                            FluidStack fluidStack = cap.extractFluid(handler.getRemainingFluidCapacity(index, null));
-                            FluidStack remainder = handler.insertFluid(fluidStack, index, null);
-                            if (remainder != null && remainder.amount > 0) {
-                                cap.insertFluid(remainder);
-                            }
-                        } else if (invFluid.amount >= handler.getFluidCapacity(0, null)) {
-                            if (cap.canInsertFluid() && handler.canExtractFluid(null)) {
-                                FluidStack fluidStack = handler.extractFluid(index, cap.getRemainingFluidCapacity(0), null);
-                                FluidStack remainder = cap.insertFluid(fluidStack);
-                                if (remainder != null && remainder.amount > 0) {
-                                    handler.insertFluid(remainder, index, null);
+
+                            // Check if there is enough fluid for another bucket
+                            if (inv.getFluid(index, null).amount >= invFluidStack.fluid.getBucketSize()) {
+                                FluidStack extractedStack = inv.extractFluid(index, invFluidStack.fluid.getBucketSize(), null);
+
+                                if (extractedStack != null) {
+                                    if (extractedStack.amount >= invFluidStack.fluid.getBucketSize()) {
+                                        player.inventory.getCursorStack().count--;
+                                        if (!pushItemBuffer(bucket.getFullBucketItem(invFluidStack.fluid), player)) {
+                                            iterationLimit = 0;
+                                        }
+
+                                        if (player.inventory.getCursorStack().count <= 0) {
+                                            iterationLimit = 0;
+                                        }
+
+                                        // Subtract the bucket size amount from the inventory fluid stack
+                                        extractedStack.amount -= invFluidStack.fluid.getBucketSize();
+                                    } else {
+                                        // We havent managed to retrieve enough fluid for another bucket, so we stop
+                                        iterationLimit = 0;
+                                    }
+                                }
+
+                                // This shouldnt happen, but we will check anyway
+                                if (extractedStack != null && extractedStack.amount > 0) {
+                                    NyaLib.LOGGER.warn("Excess fluids when extracing fluid for a bucket" + extractedStack);
+                                    inv.insertFluid(extractedStack, index, null);
                                 }
                             }
                         }
-                    } else if (cap.canInsertFluid() && handler.canExtractFluid(null)) {
-                        FluidStack fluidStack = handler.extractFluid(index, cap.getRemainingFluidCapacity(0), null);
-                        FluidStack remainder = cap.insertFluid(fluidStack);
-                        if (remainder != null && remainder.amount > 0) {
-                            handler.insertFluid(remainder, index, null);
+                    }
+                } else {
+                    // The bucket has fluid, try to deposit it into the slot
+
+                    // Try to form the empty bucket item 
+                    if (bucket.getEmptyBucketItem() != null) {
+                        PlayerInventory playerI = player.inventory;
+
+                        int iterationLimit = 127;
+                        while (iterationLimit-- > 0) {
+                            // If the remaining items in the cursor stack are the empty form, break
+                            if (playerI.getCursorStack() == null || playerI.getCursorStack().getItem() == bucket.getEmptyBucketItem()) {
+                                break;
+                            }
+
+                            // If the fluid wont fit, break
+                            if (inv.getRemainingFluidCapacity(index, null) < bucketFluid.getBucketSize()) {
+                                break;
+                            }
+
+                            // If the fluid cannot be inserted, break
+                            FluidStack fluidStack = new FluidStack(bucketFluid, bucketFluid.getBucketSize());
+                            if (!slot.canInsert(fluidStack)) {
+                                break;
+                            }
+
+                            // Insert the fluid
+                            FluidStack remainderStack = inv.insertFluid(fluidStack, index, null);
+                            if (remainderStack != null && remainderStack.amount > 0) {
+                                // If it was for some reason not inserted entirely, try to insert it in any slot which will accept it
+                                remainderStack = inv.insertFluid(remainderStack, null);
+                                if (remainderStack != null && remainderStack.amount > 0) {
+                                    NyaLib.LOGGER.warn("Excess fluids when inserting fluid from a bucket" + remainderStack);
+                                }
+                            }
+
+                            playerI.getCursorStack().count--;
+                            if (!pushItemBuffer(bucket.getEmptyBucketItem(), player)) {
+                                break;
+                            }
+
+                            if (playerI.getCursorStack().count <= 0) {
+                                break;
+                            }
                         }
                     }
-                } else if (cap.canInsertFluid() && handler.canExtractFluid(null)) {
-                    FluidStack fluidStack = handler.extractFluid(index, cap.getRemainingFluidCapacity(0), null);
-                    FluidStack remainder = cap.insertFluid(fluidStack);
-                    if (remainder != null && remainder.amount > 0) {
-                        handler.insertFluid(remainder, index, null);
-                    }
                 }
+
+                popItemBuffer(player);
             }
         }
 
         sendContentUpdates();
-        
+
         return slot != null ? slot.getStack() : null;
+    }
+
+    @Unique
+    private Item bufferItem = null;
+    @Unique
+    private int bufferCount = 0;
+
+    @Unique
+    private boolean pushItemBuffer(Item item, PlayerEntity player) {
+        if (bufferItem == null) {
+            bufferItem = item;
+        }
+
+        if (bufferItem == item) {
+            bufferCount++;
+        }
+
+        if (bufferCount >= item.getMaxCount()) {
+            return popItemBuffer(player);
+        }
+
+        return true;
+    }
+
+    @Unique
+    private boolean popItemBuffer(PlayerEntity player) {
+        if (bufferItem == null) {
+            return false;
+        }
+
+        ItemStack bufferStack = new ItemStack(bufferItem, bufferCount);
+
+        if (player.inventory.getCursorStack() == null || player.inventory.getCursorStack().count <= 0) {
+            player.inventory.setCursorStack(bufferStack);
+            resetItemBuffer();
+            return true;
+        } else {
+            if (player.inventory.addStack(bufferStack)) {
+                resetItemBuffer();
+                return true;
+            } else {
+                player.world.spawnEntity(new ItemEntity(player.world, player.x, player.y, player.z, bufferStack));
+                resetItemBuffer();
+                return false;
+            }
+        }
+    }
+
+    public void resetItemBuffer() {
+        bufferItem = null;
+        bufferCount = 0;
     }
 
     @Override
