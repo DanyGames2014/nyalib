bc2c730017231815bc4c75f3aa26c9463db1e2a1
Daniel Fildán <fildand@centrum.cz>
2026-01-05 14:21:36 +0100

    Move the bucket item fetching methods to be governed by the interface

--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/fluid/FluidBucket.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/fluid/FluidBucket.java
@@ -1,5 +1,11 @@
 package net.danygames2014.nyalib.fluid;
 
+import net.minecraft.item.Item;
+
 public interface FluidBucket {
     Fluid getFluid();
+    
+    Item getEmptyBucketItem();
+    
+    Item getFullBucketItem(Fluid fluid);
 }
--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/BucketItemMixin.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/mixin/fluid/BucketItemMixin.java
@@ -4,14 +4,15 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.danygames2014.nyalib.fluid.Fluid;
 import net.danygames2014.nyalib.fluid.FluidBucket;
 import net.danygames2014.nyalib.fluid.FluidRegistry;
-import net.danygames2014.nyalib.fluid.FluidStack;
 import net.danygames2014.nyalib.sound.SoundHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.block.States;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,16 +26,16 @@ public class BucketItemMixin implements FluidBucket {
     private int fluidBlockId;
 
     @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getMaterial(III)Lnet/minecraft/block/material/Material;", ordinal = 0, shift = At.Shift.BEFORE), cancellable = true)
-    public void pain(ItemStack stack, World world, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir, @Local HitResult hitResult) {
+    public void fillTheBucket(ItemStack stack, World world, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir, @Local HitResult hitResult) {
         int x = hitResult.blockX;
         int y = hitResult.blockY;
         int z = hitResult.blockZ;
 
         Fluid fluid = FluidRegistry.get(world.getBlockId(x, y, z));
-        if (fluid != null && fluid.getBucketItem() != null) {
+        if (fluid != null && this.getFullBucketItem(fluid) != null) {
             world.setBlockStateWithNotify(x, y, z, States.AIR.get());
             SoundHelper.playSound(player, fluid.getFillSound(), 1.0F, 1.0F);
-            cir.setReturnValue(new ItemStack(fluid.getBucketItem()));
+            cir.setReturnValue(new ItemStack(this.getFullBucketItem(fluid)));
         }
     }
 
@@ -60,5 +61,15 @@ public class BucketItemMixin implements FluidBucket {
     @Override
     public Fluid getFluid() {
         return FluidRegistry.get(fluidBlockId);
+    }
+
+    @Override
+    public Item getEmptyBucketItem() {
+        return Item.BUCKET;
+    }
+
+    @Override
+    public Item getFullBucketItem(Fluid fluid) {
+        return fluid.getBucketItem();
     }
 }
