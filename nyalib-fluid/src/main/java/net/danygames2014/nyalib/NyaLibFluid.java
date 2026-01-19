c906db2bba3493f784f405438b543cd7922aaf5a
Daniel Fildán <fildand@centrum.cz>
2026-01-19 21:25:01 +0100

    These are actually done, thank you

--- nyalib-fluid/src/main/java/net/danygames2014/nyalib/NyaLibFluid.java
+++ nyalib-fluid/src/main/java/net/danygames2014/nyalib/NyaLibFluid.java
@@ -7,8 +7,6 @@ public class NyaLibFluid {
     @Entrypoint.Namespace
     public static Namespace NAMESPACE;
     
-    // TODO: SimpleTank
     // TODO: ItemDrainer (unify Bucket and Handler logic with min/max withdrawal and deposit)
-    // TODO: ManagedItemHandler
     // TODO: ManagedEnergyHandler (idk about this one)
 }
