3097401594268d3fcb9c09f8d62fe3245883e8ec
Daniel Fildán <fildand@centrum.cz>
2026-01-25 00:17:44 +0100

    Remove debug prints

--- nyalib-multipart/src/main/java/net/danygames2014/nyalib/mixininterface/MultipartWorld.java
+++ nyalib-multipart/src/main/java/net/danygames2014/nyalib/mixininterface/MultipartWorld.java
@@ -6,17 +6,14 @@ import net.modificationstation.stationapi.api.util.Util;
 
 public interface MultipartWorld {
     default MultipartState getMultipartState(int x, int y, int z) {
-        System.err.println("BRUH GETMULTIPARTSTATE");
         return Util.assertImpl();
     }
 
     default boolean setMultipartState(int x, int y, int z, MultipartState multipartState) {
-        System.err.println("BRUH SETMULTIPARTSTATE");
         return Util.assertImpl();
     }
     
     default boolean addMultipartComponent(int x, int y, int z, MultipartComponent component) {
-        System.err.println("BRUH ADDMULTIPARTCOMPONENT");
         return Util.assertImpl();
     }
 }
