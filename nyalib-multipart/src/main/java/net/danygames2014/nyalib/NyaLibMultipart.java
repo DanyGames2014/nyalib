3e898c2046b21f2613130f1a416535a1ea492a32
Daniel Fildán <fildand@centrum.cz>
2026-01-25 22:59:20 +0100

    remove done todo items and remove unused mixin

--- nyalib-multipart/src/main/java/net/danygames2014/nyalib/NyaLibMultipart.java
+++ nyalib-multipart/src/main/java/net/danygames2014/nyalib/NyaLibMultipart.java
@@ -11,12 +11,6 @@ public class NyaLibMultipart {
     @Entrypoint.Logger
     public static Logger LOGGER;
     
-    // TODO: Multiplayer syncing
-    // TODO: Bounds checking
-    // TODO: BlockEntity support
-    // TODO: Fake block when no block present
-    // TODO: Swap fake block for real block when a real block is placed
     // TODO: Rendering
-    // TODO: Explosio nhandling
     // TODO: luminance?
 }
