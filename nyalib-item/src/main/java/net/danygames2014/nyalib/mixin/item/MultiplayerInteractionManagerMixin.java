6a722d3f56a9fa4c404b4d56c5c34381e307fd8f
Daniel Fildán <fildand@centrum.cz>
2026-01-25 22:55:17 +0100

    Dont send enhanced placement packet on non enhanced placement context items

--- nyalib-item/src/main/java/net/danygames2014/nyalib/mixin/item/MultiplayerInteractionManagerMixin.java
+++ nyalib-item/src/main/java/net/danygames2014/nyalib/mixin/item/MultiplayerInteractionManagerMixin.java
@@ -2,6 +2,7 @@ package net.danygames2014.nyalib.mixin.item;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.danygames2014.nyalib.item.EnhancedPlacementContextItem;
 import net.danygames2014.nyalib.packet.EnhancedPlayerInteractBlockC2SPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MultiplayerInteractionManager;
@@ -14,6 +15,10 @@ import org.spongepowered.asm.mixin.injection.At;
 public class MultiplayerInteractionManagerMixin {
     @WrapOperation(method = "interactBlock", at = @At(value = "NEW", target = "(IIIILnet/minecraft/item/ItemStack;)Lnet/minecraft/network/packet/c2s/play/PlayerInteractBlockC2SPacket;"))
     public PlayerInteractBlockC2SPacket useEnhancedPlacementConext(int x, int y, int z, int side, ItemStack stack, Operation<PlayerInteractBlockC2SPacket> original) {
-        return new EnhancedPlayerInteractBlockC2SPacket(x, y, z, side, stack, Minecraft.INSTANCE.crosshairTarget.pos);
+        if (stack.getItem() instanceof EnhancedPlacementContextItem) {
+            return new EnhancedPlayerInteractBlockC2SPacket(x, y, z, side, stack, Minecraft.INSTANCE.crosshairTarget.pos);
+        }
+        
+        return original.call(x, y, z, side, stack);
     }
 }
