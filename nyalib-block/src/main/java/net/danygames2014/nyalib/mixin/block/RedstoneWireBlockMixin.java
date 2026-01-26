6dbe595231ce4dd5ee78b35922d7455790462e72
Daniel Fildán <fildand@centrum.cz>
2026-01-26 23:09:40 +0100

    Behold, redstone api is real

--- nyalib-block/src/main/java/net/danygames2014/nyalib/mixin/block/RedstoneWireBlockMixin.java
+++ nyalib-block/src/main/java/net/danygames2014/nyalib/mixin/block/RedstoneWireBlockMixin.java
@@ -3,22 +3,39 @@ package net.danygames2014.nyalib.mixin.block;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
 
 @Mixin(RedstoneWireBlock.class)
-public class RedstoneWireBlockMixin {
+public abstract class RedstoneWireBlockMixin {
+    @Shadow
+    private Set neighborsToUpdate;
+
     @Unique
     int powerLevel; 
     
-    // TODO: Fix updating from 0 to 1 on strong redstone not working
+    @Unique
+    boolean updated = false;
+    
+    @Unique
+    int initialLevel = 0;
     
     @WrapOperation(method = "doUpdatePower", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isPowered(III)Z"))
     public boolean trickTheCheck(World world, int x, int y, int z, Operation<Boolean> original) {
+        initialLevel = world.getBlockMeta(x,y,z);
+        
         powerLevel = world.getPowerLevel(x, y, z);
+        updated = false;
         
         return powerLevel > 0;
     }
@@ -27,9 +44,23 @@ public class RedstoneWireBlockMixin {
     public int insertPowerLevel(int original) {
         return powerLevel;
     }
+  
+    @Inject(method = "doUpdatePower", at = @At(value = "INVOKE", target = "Ljava/util/Set;add(Ljava/lang/Object;)Z", ordinal = 0))
+    public void checkIfRan2(World world, int x, int y, int z, int sourceX, int sourceY, int sourceZ, CallbackInfo ci) {
+        updated = true;   
+    }
     
-    @WrapOperation(method = "doUpdatePower", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockMeta(III)I"))
-    public int whyNotch(World instance, int x, int y, int z, Operation<Integer> original) {
-        return original.call(instance,x,y,z);
+    @SuppressWarnings("unchecked")
+    @Inject(method = "doUpdatePower", at = @At("TAIL"))
+    public void updateIfNeccessary(World world, int x, int y, int z, int sourceX, int sourceY, int sourceZ, CallbackInfo ci, @Local(ordinal = 6) int var8, @Local(ordinal = 7) int var9) {
+        if (!updated && initialLevel != world.getBlockMeta(x,y,z)) {
+            this.neighborsToUpdate.add(new BlockPos(x, y, z));
+            this.neighborsToUpdate.add(new BlockPos(x - 1, y, z));
+            this.neighborsToUpdate.add(new BlockPos(x + 1, y, z));
+            this.neighborsToUpdate.add(new BlockPos(x, y - 1, z));
+            this.neighborsToUpdate.add(new BlockPos(x, y + 1, z));
+            this.neighborsToUpdate.add(new BlockPos(x, y, z - 1));
+            this.neighborsToUpdate.add(new BlockPos(x, y, z + 1));
+        }
     }
 }
