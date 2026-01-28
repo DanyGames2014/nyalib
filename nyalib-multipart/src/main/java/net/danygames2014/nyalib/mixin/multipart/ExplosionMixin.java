28335f3d95951fb72439197ab65a4e22007d15c7
Daniel Fildán <fildand@centrum.cz>
2026-01-28 10:17:06 +0100

    Mark a field unique in Mixin

--- nyalib-multipart/src/main/java/net/danygames2014/nyalib/mixin/multipart/ExplosionMixin.java
+++ nyalib-multipart/src/main/java/net/danygames2014/nyalib/mixin/multipart/ExplosionMixin.java
@@ -9,14 +9,20 @@ import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Explosion.class)
 public class ExplosionMixin {
-    @Shadow private World world;
-    @Shadow public Entity source;
+    @Shadow 
+    private World world;
+    
+    @Shadow 
+    public Entity source;
+    
+    @Unique
     private ObjectOpenHashSet<MultipartComponent> damagedComponents = new ObjectOpenHashSet<>();
 
     @Inject(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockId(III)I", ordinal = 0))
