927477eb2b759ba5e9093a94ee85e4519c998d0d
Daniel Fildán <fildand@centrum.cz>
2026-02-10 00:19:58 +0100

    Merge BoxUtils, oops

--- nyalib-base/src/main/java/net/danygames2014/nyalib/util/BoxUtil.java
+++ nyalib-base/src/main/java/net/danygames2014/nyalib/util/BoxUtil.java
@@ -1,5 +1,6 @@
 package net.danygames2014.nyalib.util;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.util.math.Box;
 import net.modificationstation.stationapi.api.util.math.Direction;
 
@@ -136,5 +137,61 @@ public class BoxUtil {
             }
         }
         return origin;
+    }
+
+    public static boolean isFullyCovered(Box target, ObjectArrayList<Box> covers) {
+        ObjectArrayList<Box> remainingPieces = new ObjectArrayList<>();
+        remainingPieces.add(target);
+
+        for (Box cover : covers) {
+            ObjectArrayList<Box> nextLevelPieces = new ObjectArrayList<>();
+            for (Box piece : remainingPieces) {
+                nextLevelPieces.addAll(subtract(piece, cover));
+            }
+            remainingPieces = nextLevelPieces;
+            if (remainingPieces.isEmpty()) return true;
+        }
+
+        return remainingPieces.isEmpty();
+    }
+
+    public static ObjectArrayList<Box> subtract(Box box, Box other) {
+        ObjectArrayList<Box> result = new ObjectArrayList<>();
+
+        if (!box.intersects(other)) {
+            result.add(box);
+            return result;
+        }
+
+        double curMinX = box.minX, curMaxX = box.maxX;
+        double curMinY = box.minY, curMaxY = box.maxY;
+        double curMinZ = box.minZ, curMaxZ = box.maxZ;
+
+        if (other.minX > curMinX) {
+            result.add(Box.create(curMinX, curMinY, curMinZ, other.minX, curMaxY, curMaxZ));
+            curMinX = other.minX;
+        }
+        if (other.maxX < curMaxX) {
+            result.add(Box.create(other.maxX, curMinY, curMinZ, curMaxX, curMaxY, curMaxZ));
+            curMaxX = other.maxX;
+        }
+
+        if (other.minY > curMinY) {
+            result.add(Box.create(curMinX, curMinY, curMinZ, curMaxX, other.minY, curMaxZ));
+            curMinY = other.minY;
+        }
+        if (other.maxY < curMaxY) {
+            result.add(Box.create(curMinX, other.maxY, curMinZ, curMaxX, curMaxY, curMaxZ));
+            curMaxY = other.maxY;
+        }
+
+        if (other.minZ > curMinZ) {
+            result.add(Box.create(curMinX, curMinY, curMinZ, curMaxX, curMaxY, other.minZ));
+        }
+        if (other.maxZ < curMaxZ) {
+            result.add(Box.create(curMinX, curMinY, other.maxZ, curMaxX, curMaxY, curMaxZ));
+        }
+
+        return result;
     }
 }
