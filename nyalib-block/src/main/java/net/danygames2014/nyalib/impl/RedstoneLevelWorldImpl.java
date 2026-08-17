252e80beb0c1680dd432e41bfad3d4184a888bcc
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-08-17 23:28:00 +0200

    Move redstone api impl to separate class to allow nyalib multipart to mixin into it

---
+++ nyalib-block/src/main/java/net/danygames2014/nyalib/impl/RedstoneLevelWorldImpl.java
@@ -0,0 +1,138 @@
+package net.danygames2014.nyalib.impl;
+
+import net.danygames2014.nyalib.block.RedstoneLevelProvider;
+import net.danygames2014.nyalib.mixininterface.RedstoneLevelWorld;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+
+public class RedstoneLevelWorldImpl implements RedstoneLevelWorld{
+    private final World world;
+
+    public RedstoneLevelWorldImpl(World world) {
+        this.world = world;
+    }
+
+    @Override
+    public int getStrongPowerLevelOnSide(int x, int y, int z, int side) {
+        int blockId = world.getBlockId(x, y, z);
+
+        if (blockId == 0) {
+            return 0;
+        }
+
+        Block block = Block.BLOCKS[blockId];
+
+        if (block instanceof RedstoneLevelProvider redstoneLevelProvider) {
+            return redstoneLevelProvider.getSideStrongPowerLevel(World.class.cast(this), x, y, z, side);
+        } else {
+            return block.isStrongPoweringSide(World.class.cast(this), x, y, z, side) ? 15 : 0;
+        }
+    }
+
+    @Override
+    public int getStrongPowerLevel(int x, int y, int z) {
+        int highestLevel = 0;
+        int level;
+
+        level = this.getStrongPowerLevelOnSide(x, y - 1, z, 0);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getStrongPowerLevelOnSide(x, y + 1, z, 1);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getStrongPowerLevelOnSide(x, y, z - 1, 2);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getStrongPowerLevelOnSide(x, y, z + 1, 3);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getStrongPowerLevelOnSide(x - 1, y, z, 4);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getStrongPowerLevelOnSide(x + 1, y, z, 5);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        return highestLevel;
+    }
+
+    @Override
+    public int getPowerLevelOnSide(int x, int y, int z, int side) {
+        int powerLevel = 0;
+
+        if (world.shouldSuffocate(x, y, z)) {
+            powerLevel = this.getStrongPowerLevel(x, y, z);
+        }
+
+        int blockId = world.getBlockId(x, y, z);
+
+        if (blockId == 0) {
+            return 0;
+        }
+
+        Block block = Block.BLOCKS[blockId];
+
+        if (block instanceof RedstoneLevelProvider redstoneLevelProvider) {
+            int level = redstoneLevelProvider.getSidePowerLevel(World.class.cast(this), x, y, z, side);
+            if (level > powerLevel) {
+                powerLevel = level;
+            }
+        } else {
+            int level = block.isPoweringSide(World.class.cast(this), x, y, z, side) ? 15 : 0;
+            if (level > powerLevel) {
+                powerLevel = level;
+            }
+        }
+
+        return powerLevel;
+    }
+
+    @Override
+    public int getPowerLevel(int x, int y, int z) {
+        int highestLevel = 0;
+        int level;
+
+        level = this.getPowerLevelOnSide(x, y - 1, z, 0);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getPowerLevelOnSide(x, y + 1, z, 1);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getPowerLevelOnSide(x, y, z - 1, 2);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getPowerLevelOnSide(x, y, z + 1, 3);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getPowerLevelOnSide(x - 1, y, z, 4);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        level = this.getPowerLevelOnSide(x + 1, y, z, 5);
+        if (level > highestLevel) {
+            highestLevel = level;
+        }
+
+        return highestLevel;
+    }
+}
--- nyalib-block/src/main/java/net/danygames2014/nyalib/mixin/block/WorldMixin.java
+++ nyalib-block/src/main/java/net/danygames2014/nyalib/mixin/block/WorldMixin.java
@@ -1,12 +1,14 @@
 package net.danygames2014.nyalib.mixin.block;
 
 import net.danygames2014.nyalib.block.RedstoneLevelProvider;
+import net.danygames2014.nyalib.impl.RedstoneLevelWorldImpl;
 import net.danygames2014.nyalib.mixininterface.RedstoneLevelWorld;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.world.StationFlatteningWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,6 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @SuppressWarnings("AddedMixinMembersNamePattern")
 @Mixin(World.class)
 public abstract class WorldMixin implements RedstoneLevelWorld, StationFlatteningWorld {
+
+    @Unique
+    private final RedstoneLevelWorldImpl redstoneLevelWorldImpl = new RedstoneLevelWorldImpl((World) (Object)this);
+
     @Shadow
     public abstract int getBlockId(int x, int y, int z);
 
@@ -22,19 +28,7 @@ public abstract class WorldMixin implements RedstoneLevelWorld, StationFlattenin
 
     @Override
     public int getStrongPowerLevelOnSide(int x, int y, int z, int side) {
-        int blockId = this.getBlockId(x, y, z);
-
-        if (blockId == 0) {
-            return 0;
-        }
-
-        Block block = Block.BLOCKS[blockId];
-
-        if (block instanceof RedstoneLevelProvider redstoneLevelProvider) {
-            return redstoneLevelProvider.getSideStrongPowerLevel(World.class.cast(this), x, y, z, side);
-        } else {
-            return block.isStrongPoweringSide(World.class.cast(this), x, y, z, side) ? 15 : 0;
-        }
+        return redstoneLevelWorldImpl.getStrongPowerLevelOnSide(x, y, z, side);
     }
 
     @Inject(method = "isStrongPoweringSide", at = @At(value = "HEAD"), cancellable = true)
@@ -44,40 +38,7 @@ public abstract class WorldMixin implements RedstoneLevelWorld, StationFlattenin
 
     @Override
     public int getStrongPowerLevel(int x, int y, int z) {
-        int highestLevel = 0;
-        int level;
-
-        level = this.getStrongPowerLevelOnSide(x, y - 1, z, 0);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getStrongPowerLevelOnSide(x, y + 1, z, 1);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getStrongPowerLevelOnSide(x, y, z - 1, 2);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getStrongPowerLevelOnSide(x, y, z + 1, 3);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getStrongPowerLevelOnSide(x - 1, y, z, 4);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getStrongPowerLevelOnSide(x + 1, y, z, 5);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        return highestLevel;
+        return redstoneLevelWorldImpl.getStrongPowerLevel(x, y, z);
     }
 
     @Inject(method = "isStrongPowered", at = @At(value = "HEAD"), cancellable = true)
@@ -87,33 +48,7 @@ public abstract class WorldMixin implements RedstoneLevelWorld, StationFlattenin
 
     @Override
     public int getPowerLevelOnSide(int x, int y, int z, int side) {
-        int powerLevel = 0;
-
-        if (this.shouldSuffocate(x, y, z)) {
-            powerLevel = this.getStrongPowerLevel(x, y, z);
-        }
-
-        int blockId = this.getBlockId(x, y, z);
-
-        if (blockId == 0) {
-            return 0;
-        }
-
-        Block block = Block.BLOCKS[blockId];
-
-        if (block instanceof RedstoneLevelProvider redstoneLevelProvider) {
-            int level = redstoneLevelProvider.getSidePowerLevel(World.class.cast(this), x, y, z, side);
-            if (level > powerLevel) {
-                powerLevel = level;
-            }
-        } else {
-            int level = block.isPoweringSide(World.class.cast(this), x, y, z, side) ? 15 : 0;
-            if (level > powerLevel) {
-                powerLevel = level;
-            }
-        }
-        
-        return powerLevel;
+        return redstoneLevelWorldImpl.getPowerLevelOnSide(x, y, z, side);
     }
 
     @Inject(method = "isPoweringSide", at = @At(value = "HEAD"), cancellable = true)
@@ -123,40 +58,7 @@ public abstract class WorldMixin implements RedstoneLevelWorld, StationFlattenin
 
     @Override
     public int getPowerLevel(int x, int y, int z) {
-        int highestLevel = 0;
-        int level;
-
-        level = this.getPowerLevelOnSide(x, y - 1, z, 0);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getPowerLevelOnSide(x, y + 1, z, 1);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getPowerLevelOnSide(x, y, z - 1, 2);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getPowerLevelOnSide(x, y, z + 1, 3);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getPowerLevelOnSide(x - 1, y, z, 4);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        level = this.getPowerLevelOnSide(x + 1, y, z, 5);
-        if (level > highestLevel) {
-            highestLevel = level;
-        }
-
-        return highestLevel;
+        return redstoneLevelWorldImpl.getPowerLevel(x, y, z);
     }
 
     @Inject(method = "isPowered", at = @At(value = "HEAD"), cancellable = true)
